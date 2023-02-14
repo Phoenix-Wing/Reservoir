4ad7c4d6b0254728a49f6203bf6b69c7257539de
BD103 <59022059+BD103@users.noreply.github.com>
2023-02-14 14:19:05 -0500

    feat: add nuxtjs

---
+++ app.vue
@@ -0,0 +1,13 @@
+<template>
+    <main>
+        <h1>Reservoir</h1>
+
+        <h2>Browse Countries</h2>
+
+        <ul>
+            <li>Country Name - Leader Name (IRL Name)</li>
+            <li>Country Name - Leader Name (IRL Name)</li>
+            <li>Country Name - Leader Name (IRL Name)</li>
+        </ul>
+    </main>
+</template>
