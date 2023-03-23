e9535a9d14fa3ff88199b132436e92013a6d62cf
BD103 <59022059+BD103@users.noreply.github.com>
2023-03-23 10:31:43 -0400

    refactor: rename some components

--- pages/country/[id]/index.vue
+++ pages/country/[id]/index.vue
@@ -21,9 +21,9 @@
 
                     <NCard title="Quick facts">
                         <NSpace justify="space-around">
-                                <FancyStatistic :to="country.gold_store" label="Total Gold" suffix="g" />
-                                <FancyStatistic :to="country.material_store" label="Total Materials" suffix="mat" />
-                                <FancyStatistic :to="country.population" label="Population" suffix="people" />
+                                <ReStat :to="country.gold_store" label="Total Gold" suffix="g" />
+                                <ReStat :to="country.material_store" label="Total Materials" suffix="mat" />
+                                <ReStat :to="country.population" label="Population" suffix="people" />
                         </NSpace>
                     </NCard>
                 </NPageHeader>
@@ -33,62 +33,62 @@
                 <NGrid :cols="2" :x-gap="12" :y-gap="12">
                     <!-- Gold card -->
                     <NGi>
-                        <CountryStats title="Gold">
+                        <ReStatCard title="Gold">
                             <NGi>
-                                <FancyStatistic :to="country.gold_store" label="Total" suffix="g" />
+                                <ReStat :to="country.gold_store" label="Total" suffix="g" />
                             </NGi>
                             <NGi>
                                 <NPopover trigger="hover">
                                     <template #trigger>
-                                        <FancyStatistic :to="country.gold_profit" label="Profit" suffix="g" />
+                                        <ReStat :to="country.gold_profit" label="Profit" suffix="g" />
                                     </template>
 
                                     <span>Calculated with <code>income - upkeep</code></span>
                                 </NPopover>
                             </NGi>
                             <NGi>
-                                <FancyStatistic :to="country.gold_income" label="Income" suffix="g" />
+                                <ReStat :to="country.gold_income" label="Income" suffix="g" />
                             </NGi>
                             <NGi>
-                                <FancyStatistic :to="country.gold_upkeep" label="Upkeep" suffix="g" />
+                                <ReStat :to="country.gold_upkeep" label="Upkeep" suffix="g" />
                             </NGi>
 
                             <template #description>Gold is the universal currency in Phoenix Wing.</template>
-                        </CountryStats>
+                        </ReStatCard>
                     </NGi>
 
                     <!-- Material card -->
                     <NGi>
-                        <CountryStats title="Materials">
+                        <ReStatCard title="Materials">
                             <NGi>
-                                <FancyStatistic :to="country.material_store" label="Total" suffix="mat" />
+                                <ReStat :to="country.material_store" label="Total" suffix="mat" />
                             </NGi>
                             <NGi>
                                 <NPopover trigger="hover">
                                     <template #trigger>
-                                        <FancyStatistic :to="country.material_profit" label="Profit" suffix="mat" />
+                                        <ReStat :to="country.material_profit" label="Profit" suffix="mat" />
                                     </template>
 
                                     <span>Calculated with <code>income - upkeep</code></span>
                                 </NPopover>
                             </NGi>
                             <NGi>
-                                <FancyStatistic :to="country.material_income" label="Income" suffix="mat" />
+                                <ReStat :to="country.material_income" label="Income" suffix="mat" />
                             </NGi>
                             <NGi>
-                                <FancyStatistic :to="country.material_upkeep" label="Upkeep" suffix="mat" />
+                                <ReStat :to="country.material_upkeep" label="Upkeep" suffix="mat" />
                             </NGi>
 
                             <template #description>
                                 Materials are used to build new structures.
                             </template>
-                        </CountryStats>
+                        </ReStatCard>
                     </NGi>
 
                     <NGi>
                         <NCard title="Population">
                             <NSpace justify="center">
-                                <FancyStatistic :to="country.population" label="Total" suffix="people" />
+                                <ReStat :to="country.population" label="Total" suffix="people" />
                             </NSpace>
 
                             <template #action>
@@ -103,23 +103,23 @@
                         <NSpace vertical :size="24">
                             <NCard title="Gold">
                                 <NForm inline>
-                                    <FormNumberField @update="x => editArgs.gold_store = x" :default="country.gold_store" label="Total" />
-                                    <FormNumberField @update="x => editArgs.gold_income = x" :default="country.gold_income" label="Income" />
-                                    <FormNumberField @update="x => editArgs.gold_upkeep = x" :default="country.gold_upkeep" label="Upkeep" />
+                                    <ReNumField @update="x => editArgs.gold_store = x" :default="country.gold_store" label="Total" />
+                                    <ReNumField @update="x => editArgs.gold_income = x" :default="country.gold_income" label="Income" />
+                                    <ReNumField @update="x => editArgs.gold_upkeep = x" :default="country.gold_upkeep" label="Upkeep" />
                                 </NForm>
                             </NCard>
 
                             <NCard title="Materials">
                                 <NForm inline>
-                                    <FormNumberField @update="x => editArgs.material_store = x" :default="country.material_store" label="Total" />
-                                    <FormNumberField @update="x => editArgs.material_income = x" :default="country.material_income" label="Income" />
-                                    <FormNumberField @update="x => editArgs.material_upkeep = x" :default="country.material_upkeep" label="Upkeep" />
+                                    <ReNumField @update="x => editArgs.material_store = x" :default="country.material_store" label="Total" />
+                                    <ReNumField @update="x => editArgs.material_income = x" :default="country.material_income" label="Income" />
+                                    <ReNumField @update="x => editArgs.material_upkeep = x" :default="country.material_upkeep" label="Upkeep" />
                                 </NForm>
                             </NCard>
 
                             <NCard title="Population">
                                 <NForm inline>
-                                    <FormNumberField @update="x => editArgs.population = x" :default="country.population" label="Total" />
+                                    <ReNumField @update="x => editArgs.population = x" :default="country.population" label="Total" />
                                 </NForm>
                             </NCard>
                         </NSpace>
