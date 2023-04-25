0e88fc7055d2cdea66f71f19df61619aff5c5eab
BD103 <59022059+BD103@users.noreply.github.com>
2023-04-25 13:34:15 -0400

    feat: handle more edge cases with member list

--- components/MemberList.vue
+++ components/MemberList.vue
@@ -1,28 +1,34 @@
 <template>
-    <NList bordered hoverable>
-        <template #header>
-            <NH2>Browse members</NH2>
-        </template>
-
-        <NListItem v-for="member in data?.members">
-            <template #prefix>
-                {{ member.name }}
+    <NSpin :show="pending">
+        <NList bordered hoverable>
+            <template #header>
+                <NH2>Browse members</NH2>
             </template>
 
-            <NText :depth="3">
-                {{ member.ig_name ? `${member.ig_name},` : "" }}
-                {{ member.countries.length == 1 ? `${member.countries.length} country` : `${member.countries.length} countries` }}
-            </NText>
+            <ErrorDisplay v-if="error" description="Error fetching members." :details="error" style="margin: 1rem 0" />
 
-            <template #suffix>
-                <NButton @click="navigateTo(`/member/${member.id}`)">View</NButton>
+            <NListItem v-if="data?.members.length != 0" v-for="member in data?.members">
+                <template #prefix>
+                    {{ member.name }}
+                </template>
+
+                <NText :depth="3">
+                    {{ member.ig_name ? `${member.ig_name},` : "" }}
+                    {{ member.countries.length == 1 ? `${member.countries.length} country` : `${member.countries.length} countries` }}
+                </NText>
+
+                <template #suffix>
+                    <NButton @click="navigateTo(`/member/${member.id}`)">View</NButton>
+                </template>
+            </NListItem>
+
+            <NEmpty v-else size="huge" description="No members yet..." style="margin: 1rem 0" />
+
+            <template #footer>
+                <NPagination v-model:page="page" :item-count="data?.total" :page-size="PAGE_SIZE" :disabled="error != null" />
             </template>
-        </NListItem>
-
-        <template #footer>
-            <NPagination v-model:page="page" :item-count="data?.total" :page-size="PAGE_SIZE" />
-        </template>
-    </NList>
+        </NList>
+    </NSpin>
 </template>
 
 <script setup lang="ts">
@@ -35,7 +41,7 @@ const memberQuery = computed(() => ({
     offset: (page.value - 1) * PAGE_SIZE,
 }));
 
-const { data } = await useFetch("/api/members", {
+const { data, pending, error } = await useLazyFetch("/api/members", {
     query: memberQuery,
 });
 </script>
