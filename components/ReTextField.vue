2ab78c64eb35fc109ebb028ca17793d27e20754f
BD103 <59022059+BD103@users.noreply.github.com>
2023-05-04 09:59:36 -0400

    feat: text field component

---
+++ components/ReTextField.vue
@@ -0,0 +1,30 @@
+<template>
+    <NFormItem :label="label">
+        <NInput type="text" @update:value=" emitUpdate " :defaultValue="default" :maxlength=" maxlength " :showCount=" showCount "
+            clearable />
+    </NFormItem>
+</template> 
+
+<script setup lang="ts">
+const props = defineProps<{
+    default: string,
+    label?: string,
+    maxlength?: string,
+}>();
+
+const emit = defineEmits<{
+    (event: "update", val: string | null): void,
+}>();
+
+function emitUpdate(val: string | null) {
+    if (val != props.default) {
+        emit("update", val);
+    } else {
+        emit("update", null);
+    }
+}
+
+const showCount = computed(() => {
+    return props.maxlength ? true : false;
+});
+</script>
