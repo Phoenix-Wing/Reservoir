77c427291353447521e8196b8031d78b6141a915
BD103 <59022059+BD103@users.noreply.github.com>
2023-05-11 08:59:49 -0400

    refactor: make member list consistent

--- components/home/MemberList.vue
+++ components/home/MemberList.vue
@@ -7,7 +7,7 @@
 
             <ErrorDisplay v-if="error" description="Error fetching members." :details="error" style="margin: 1rem 0" />
 
-            <NListItem v-if="data?.members.length != 0" v-for="member in data?.members">
+            <NListItem v-else-if="data!.total > 0" v-for="member in data?.members">
                 <template #prefix>
                     {{ member.name }}
                 </template>
