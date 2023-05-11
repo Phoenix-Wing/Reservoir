07038318bd1c61c398488adb95ea9e8d7583a4db
BD103 <59022059+BD103@users.noreply.github.com>
2023-05-11 08:58:58 -0400

    feat: country list pagination

--- components/home/CountryList.vue
+++ components/home/CountryList.vue
@@ -1,45 +1,52 @@
 <template>
-    <NSpin v-if="pending">
-        <!-- Loading spinner around empty list -->
-
-        <NList bordered>
+    <NSpin :show="pending">
+        <NList bordered hoverable>
             <template #header>
                 <NH2>Browse countries</NH2>
             </template>
+
+            <!-- If error fetching data -->
+            <ErrorDisplay v-if="error" description="Error fetching countries." :details="error" style="margin: 1rem 0" />
+
+            <!-- Else if more than 0 countries -->
+            <NListItem v-else-if="data!.total > 0" v-for="country in data?.countries">
+                <template #prefix>
+                    <NText type="success" strong>{{ country.name }}</NText>
+                </template>
+
+                <div v-if="country.leaders">
+                    <ULeadersDisplay :leaders="country.leaders" color="rgba(255, 255, 255, 0.9)" />
+                </div>
+                <NText v-else italic>
+                    No current leaders
+                </NText>
+
+                <template #suffix>
+                    <UButtonLink :to="`/country/${country.id}`">View</UButtonLink>
+                </template>
+            </NListItem>
+
+            <!-- Else, countries.length must be 0 -->
+            <NEmpty v-else size="huge" description="No countries yet..." style="margin: 1rem 0" />
+
+            <template #footer>
+                <NPagination v-model:page="page" :item-count="data?.total" :page-size="PAGE_SIZE" :disabled="error != null" />
+            </template>
         </NList>
     </NSpin>
-
-    <NList v-else bordered hoverable>
-        <template #header>
-            <NH2>Browse countries</NH2>
-        </template>
-
-        <!-- If error fetching data -->
-        <ErrorDisplay v-if="error" description="Error fetching countries." :details="error" style="margin: 1rem 0" />
-
-        <!-- Else if more than 0 countries -->
-        <NListItem v-else-if="data!.length > 0" v-for="country in data">
-            <template #prefix>
-                <NText type="success" strong>{{ country.name }}</NText>
-            </template>
-
-            <div v-if="country.leaders">
-                <ULeadersDisplay :leaders="country.leaders" color="rgba(255, 255, 255, 0.9)" />
-            </div>
-            <NText v-else italic>
-                No current leaders
-            </NText>
-
-            <template #suffix>
-                <UButtonLink :to="`/country/${country.id}`">View</UButtonLink>
-            </template>
-        </NListItem>
-
-        <!-- Else, countries.length must be 0 -->
-        <NEmpty v-else size="huge" description="No countries yet..." style="margin: 1rem 0" />
-    </NList>
 </template>
 
 <script setup lang="ts">
-const { data, pending, error } = await useFetch("/api/countries");
+const PAGE_SIZE = 8;
+
+const page = ref(1);
+
+const countryQuery = computed(() => ({
+    limit: PAGE_SIZE,
+    offset: (page.value - 1) * PAGE_SIZE,
+}));
+
+const { data, pending, error } = await useFetch("/api/countries", {
+    query: countryQuery,
+});
 </script>
--- server/api/countries.get.ts
+++ server/api/countries.get.ts
@@ -2,18 +2,45 @@ import * as edgedb from "edgedb";
 import e from "~/dbschema/edgeql-js";
 
 const client = edgedb.createClient();
-const query = e.select(e.Country, country => ({
-    id: true,
-    name: true,
-    leaders: {
-        id: true,
-        name: true,
-        ig_name: true,
+const query = e.params(
+    {
+        offset: e.optional(e.int64),
+        limit: e.optional(e.int64),
     },
+    (params) => e.select({
+        total: e.count(e.select(e.Country)),
+        countries: e.select(e.Country, country => ({
+            id: true,
+            name: true,
+            leaders: {
+                id: true,
+                name: true,
+                ig_name: true,
+            },
+        
+            order_by: country.name,
+            offset: params.offset,
+            limit: params.limit,
+        })),
+    }),
+);
 
-    order_by: country.name,
-}));
+export default defineEventHandler(async (event) => {
+    const args = getQuery(event);
 
-export default defineEventHandler(async () => {
-    return await query.run(client);
+    let offset = null;
+    let limit = null;
+
+    if (typeof args.offset == "string") {
+        offset = parseInt(args.offset);
+    }
+
+    if (typeof args.limit == "string") {
+        limit = parseInt(args.limit);
+    }
+
+    return await query.run(client, {
+        offset,
+        limit,
+    });
 });
