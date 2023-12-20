037119fc1f708a8d46d84a4e614ca121fb84a9ae
BD103 <59022059+BD103@users.noreply.github.com>
2023-12-20 13:19:26 -0500

    feat: add footer to all pages

---
+++ components/NLink.vue
@@ -0,0 +1,11 @@
+<template>
+    <NuxtLink class="n-link">
+        <slot />
+    </NuxtLink>
+</template>
+
+<style scoped>
+.n-link {
+    color: v-bind("useThemeVars().value.primaryColor");
+}
+</style>
