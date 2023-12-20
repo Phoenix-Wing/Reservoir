4b2506f328091345aad97714f6bb169c3aee735b
BD103 <59022059+BD103@users.noreply.github.com>
2023-12-20 11:53:02 -0500

    feat: make title a gradient

--- pages/index.vue
+++ pages/index.vue
@@ -3,9 +3,9 @@
         <NSpace justify="center" style="padding: 1rem 0">
             <NH1>
                 <NuxtLink to="/" style="text-decoration: none">
-                    <NText type="primary" style="font-size: 40pt">
+                    <NGradientText type="primary" size="40pt">
                         Reservoir
-                    </NText>
+                    </NGradientText>
                 </NuxtLink>
             </NH1>
         </NSpace>
