f6a7318b236a2c5e0d4a770ea9cb93686fb7aeb5
BD103 <59022059+BD103@users.noreply.github.com>
2023-12-20 11:35:12 -0500

    fix: data is null error in member list

--- components/home/MemberList.vue
+++ components/home/MemberList.vue
@@ -7,7 +7,7 @@
 
             <ErrorDisplay v-if="error" description="Error fetching members." :details="error" style="margin: 1rem 0" />
 
-            <NListItem v-for="member in data?.members" v-else-if="data!.total > 0" :key="member.id">
+            <NListItem v-for="member in data?.members" v-else-if="(data ? data.total : 0) > 0" :key="member.id">
                 <template #prefix>
                     {{ member.name }}
                 </template>
